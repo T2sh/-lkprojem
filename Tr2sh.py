fae903a6e1f93ca451c9c0046d9a98e081dd9e4b
T2sh <138707157+T2sh@users.noreply.github.com>
2023-07-05 20:19:33 +0300

    Update Tr2sh.py

--- Tr2sh.py
+++ Tr2sh.py
@@ -1,2 +1,11 @@
-modern_sozluk = {"LOL":"gulmekten karnı yarılmak","CRINGE":"utandırıcı olay ya da nesneler","ROFL":"esprıtuel cevap"
-print (modern_sozluk [])
+modern_sozluk = { "LOL":"sesli gulmek", "ROFL":"gulmekten yere donmek", "CRINGE":"Utandirici"}
+
+soru = input("Hangi kelimenin anlami ogrenmek istiyorsun?")
+
+soru = soru.upper()
+
+if soru in modern_sozluk.keys():
+    print(modern_sozluk[soru])
+else:
+    # Kelime eşleşmiyorsa ne yapmalıyız?
+    print("Kelime bulunmadi!")
